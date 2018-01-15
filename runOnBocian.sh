71ab7737d02df17143cff7463b5fff0b03702700
Jenkins CI Server <jenkins@bocian.inf.ed.ac.uk>
2018-01-15 13:04:23 +0000

    script fix

--- runOnBocian.sh
+++ runOnBocian.sh
@@ -1,7 +1,7 @@
 cd /partition/bocian/VFBTools/DrosLarvalDomains/
 git pull
 source ../python-modules-2.6/bin/activate
-python /disk/data/VFB/IMAGE_DATA/StackProcessing/scripts/nrrd2wlzDom.py individualDomainFiles/domain template/template.wlz ../Woolz2013Full/bin/ '/disk/data/VFBTools/Fiji/ImageJ-linux64 --headless' 0.293 0.5
+python /partition/bocian/VFBTools/IndexStackConv/nrrd2wlzDom.py individualDomainFiles/domain template/template.wlz ../Woolz2013Full/bin/ '/disk/data/VFBTools/Fiji/ImageJ-linux64 --headless' 0.293 0.5
 deactivate
 mv out.wlz combinedIndexFiles/composite.wlz
 
